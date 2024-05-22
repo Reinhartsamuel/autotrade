77086d76052327c31edbcda264433de2c8b564cf
Reinhart Samuel <106574869+Reinhartsamuel@users.noreply.github.com>
2024-05-22 14:28:59 +0700

    change key json to base64

--- app/api/route.js
+++ app/api/route.js
@@ -1,7 +1,11 @@
-export async function GET() {
+import { db } from '../../lib/firebase-admin-config';
 
-    return Response.json({
-        status : true, 
-        message : "selamat datang"
-    })
-  }
+export async function GET() {
+  const doc = await db.collection("users").doc("wImZr1fZmkQGazgzcMSk3i1cMDu1").get();
+  if (!doc.exists) return Response.json({status : false, message : "user not found", uid :"wImZr1fZmkQGazgzcMSk3i1cMDu1" })
+  return Response.json({
+    status: true,
+    message: "selamat datang",
+    data : {...doc.data(), id : doc.id}
+  });
+}
