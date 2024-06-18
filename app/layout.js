2a701dc16977038a29d2e457d09084b29d02431c
Reinhart Samuel <106574869+Reinhartsamuel@users.noreply.github.com>
2024-06-18 17:10:04 +0800

    add suspense

--- app/layout.js
+++ app/layout.js
@@ -3,6 +3,7 @@ import "./globals.css";
 import { Providers } from "./providers/Providers";
 import NavbarChakra from './components/NavbarChakra';
 import Footer from './components/Footer';
+import { Suspense } from 'react';
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -15,12 +16,14 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={inter.className}>
-        <Providers>
-          {/* <Navbar /> */}
-          <NavbarChakra />
-          {children}
-          <Footer />
-        </Providers>
+        <Suspense>
+          <Providers>
+            {/* <Navbar /> */}
+            <NavbarChakra />
+            {children}
+            <Footer />
+          </Providers>
+        </Suspense>
       </body>
     </html>
   );
