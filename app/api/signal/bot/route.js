1b934214e7d670e1ae075db8e4531cedd07a5c21
Reinhart Samuel <106574869+Reinhartsamuel@users.noreply.github.com>
2024-08-12 22:02:09 +0700

    add save to db 3commas post request

--- app/api/signal/bot/route.js
+++ app/api/signal/bot/route.js
@@ -83,6 +83,11 @@ export async function POST(request) {
         });
         if (parseInt(i) == 0) console.log(sendBodyTo3Commas, 'sendBodyTo3Commas');
         const returnValue = await res.json();
+        await adminDb.collection('3commas_logs').add({
+          ...sendBodyTo3Commas,
+          createdAt: new Date(),
+          returnValue
+        });
         return returnValue;
       })
     );
